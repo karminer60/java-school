6b91eba918fffeb01eebc665fe0629449e0e6bdc
karina-rodriguez <karminer60@gmail.com>
2020-07-20 23:18:10 -0700

    edited code to include new exceptions

--- schools/src/main/java/com/lambdaschool/schools/services/HelperFunctionsImpl.java
+++ schools/src/main/java/com/lambdaschool/schools/services/HelperFunctionsImpl.java
@@ -9,33 +9,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service(value = "helperFunctions")
-public class HelperFunctionsImpl implements HelperFunctions
-{
+public class HelperFunctionsImpl implements HelperFunctions {
     @Override
-    public List<ValidationError> getConstraintViolation(Throwable cause)
-    {
-        while((cause != null) !(cause instanceof ConstraintViolationException)
-    {
-        cause = cause.getCause();
-    }
-
-
-    List<ValidationError> listVE = new ArrayList<>();
-
-    if(cause != null)
-    {
-        ConstraintViolationException ex = (ConstraintViolationException) cause;
-        for(ConstraintViolation cv: ex.getConstraintViolations())
+    public List<ValidationError> getConstraintViolation(Throwable cause) {
+        while ((cause != null) && !(cause instanceof ConstraintViolationException))
         {
-            ValidationError newVe = new ValidationError();
-            newVe.setCode(cv.getInvalidValue().toString());
-            newVe.setMessage(cv.getMessage());
-            listVE.add(newVe);
-        }
+            cause = cause.getCause();
         }
 
 
+        List<ValidationError> listVE = new ArrayList<>();
 
-    }
+        if (cause != null) {
+            ConstraintViolationException ex = (ConstraintViolationException) cause;
+            for (ConstraintViolation cv : ex.getConstraintViolations()) {
+                ValidationError newVe = new ValidationError();
+                newVe.setCode(cv.getInvalidValue().toString());
+                newVe.setMessage(cv.getMessage());
+                listVE.add(newVe);
+
+            }
+
+
+        }
         return listVE;
+    }
 }
