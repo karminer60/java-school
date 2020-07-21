2462c29144e619cd63c38ee43e56690b235e04e4
karina-rodriguez <karminer60@gmail.com>
2020-07-21 10:48:55 -0700

    I think I have met MVP

--- schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
+++ schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
@@ -14,12 +14,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
-public class RestExceptionHandler
+public class RestExceptionHandler extends ResponseEntityExceptionHandler
 {
     @Autowired
     private HelperFunctions helperFunctions;
